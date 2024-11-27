1c9f265b000e94fafcf72ca721ee92c6707a31f2
Pragyan Coder <107480064+PragyanCoder@users.noreply.github.com>
2024-11-27 07:22:25 +0530

    Update stats.py

--- pragyan/modules/stats.py
+++ pragyan/modules/stats.py
@@ -32,3 +32,53 @@ async def stats(client, message):
 
 **__Powered by Pragyan__**
 """)
+
+@app.on_message(filters.command("speedtest"))
+async def speedtest(client, message):
+    speed = await message.reply("Running Speed Test. Please wait a moment...")
+
+    # Initialize Speedtest object
+    test = Speedtest()
+    test.get_best_server()  # Get the best server for the test
+
+    # Perform download and upload speed tests
+    download_speed = test.download()
+    upload_speed = test.upload()
+
+    # Wait for both tests to complete (using `test.results` to get results after both tests)
+    test_results = test.results.dict()
+
+    # Format the output message
+    string_speed = f'''
+╭─《 🚀 SPEEDTEST INFO 》
+├ <b>Upload:</b> <code>{speed_convert(download_speed, False)}</code>
+├ <b>Download:</b>  <code>{speed_convert(upload_speed, False)}</code>
+├ <b>Ping:</b> <code>{test_results['ping']} ms</code>
+├ <b>Time:</b> <code>{test_results['timestamp']}</code>
+├ <b>Data Sent:</b> <code>{get_readable_file_size(int(test_results['bytes_sent']))}</code>
+╰ <b>Data Received:</b> <code>{get_readable_file_size(int(test_results['bytes_received']))}</code>
+╭─《 🌐 SPEEDTEST SERVER 》
+├ <b>Name:</b> <code>{test_results['server']['name']}</code>
+├ <b>Country:</b> <code>{test_results['server']['country']}, {test_results['server']['cc']}</code>
+├ <b>Sponsor:</b> <code>{test_results['server']['sponsor']}</code>
+├ <b>Latency:</b> <code>{test_results['server']['latency']}</code>
+├ <b>Latitude:</b> <code>{test_results['server']['lat']}</code>
+╰ <b>Longitude:</b> <code>{test_results['server']['lon']}</code>
+╭─《 👤 CLIENT DETAILS 》
+├ <b>IP Address:</b> <code>{test_results['client']['ip']}</code>
+├ <b>Latitude:</b> <code>{test_results['client']['lat']}</code>
+├ <b>Longitude:</b> <code>{test_results['client']['lon']}</code>
+├ <b>Country:</b> <code>{test_results['client']['country']}</code>
+├ <b>ISP:</b> <code>{test_results['client']['isp']}</code>
+╰ <b>ISP Rating:</b> <code>{test_results['client']['isprating']}</code>
+'''
+
+    try:
+        # Send the speed test results with the screenshot (if available)
+        await message.reply(string_speed, file=test_results['share'], parse_mode='html')
+        await speed.delete()  # Delete the initial speed test message
+    except Exception as e:
+        print(e)  # Log any errors that occur
+        await speed.delete()
+        await message.reply(string_speed, parse_mode='html')  # Send the result even if screenshot fails
+
